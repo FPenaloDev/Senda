0944aaafdebea46f569cc9bfab23351ea3b8c248
FPDev <francisco.penalo.ferrer@gmail.com>
2025-05-20 23:06:09 +0200

    vinos

---
+++ js/vinos.js
@@ -0,0 +1,18 @@
+export const vinos = [
+    {
+            selectorTitle: '.vinos .vinos1 h2',
+            selectorDescription: '.vinos .vinos1 .descripcion-plato',
+            es: {
+                title: "AZPILICUETA",
+                description: "Azpilicueta Rioja es un vino tinto elegante y equilibrado. Presenta aromas a frutas rojas maduras como cereza y ciruela, combinadas con notas especiadas, vainilla y un toque de madera. En boca es suave, con taninos redondos y un final largo y armonioso.",
+            },
+            en: {
+                title: "AZPILICUETA",
+                description: "Azpilicueta Rioja is an elegant and well-balanced red wine. It offers aromas of ripe red fruits like cherry and plum, blended with spicy notes, vanilla, and a hint of oak. On the palate, it's smooth, with rounded tannins and a long, harmonious finish.",
+            },
+            fr: {
+                title: "AZPILICUETA",
+                description: "Azpilicueta Rioja est un vin rouge élégant et bien équilibré. Il dégage des arômes de fruits rouges mûrs comme la cerise et la prune, associés à des notes épicées, de vanille et une touche de bois. En bouche, il est doux, avec des tanins ronds et une finale longue et harmonieuse.",
+            }
+        },
+];
