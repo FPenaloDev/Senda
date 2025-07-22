0bc852cd401768740418f23fcf65ad7d41e2c9a8
FPDev <francisco.penalo.ferrer@gmail.com>
2025-07-22 19:05:59 +0200

    nuevo deiseño

--- js/croquetas.js
+++ js/croquetas.js
@@ -2,101 +2,101 @@ import { alergenos } from './alergenos.js';
 
 export const croquetas = [
     {
-        selectorTitle: '.croquetas .croquetas1 h2',
-        selectorDescription: '.croquetas .croquetas1 .descripcion-plato', // Descripción del plato
-        selectorAllergens: '.croquetas .croquetas1 .texto-alergenos', // Párrafo de los alérgenos
+        selectorTitle: '.raciones .raciones22 h2',
+        selectorDescription: '.raciones .raciones22 .descripcion-plato', // Descripción del plato
+        selectorAllergens: '.raciones .raciones22 .texto-alergenos', // Párrafo de los alérgenos
         es: {
-            title: 'Jamon',
+            title: 'Croquetas de Jamon',
             description: '',
             allergens: [alergenos.gluten.es,alergenos.leche.es,alergenos.huevos.es]
         },
         en: {
-            title: 'HAM',
+            title: 'Croquettes of HAM',
             description: '',
             allergens: [alergenos.gluten.en,alergenos.leche.en,alergenos.huevos.en]
         },
         fr: {
-            title: "JAMBON",
+            title: "croquettes de JAMBON",
             description: "",
             allergens: [alergenos.gluten.fr,alergenos.leche.fr,alergenos.huevos.fr]
         }
     },
     {
-        selectorTitle: '.croquetas .croquetas2 h2',
-        selectorDescription: '.croquetas .croquetas2 .descripcion-plato',
-        selectorAllergens: '.croquetas .croquetas2 .texto-alergenos',
+        selectorTitle: '.raciones .raciones23 h2',
+        selectorDescription: '.raciones .raciones23 .descripcion-plato',
+        selectorAllergens: '.raciones .raciones23 .texto-alergenos',
         es: {
-            title: 'Queso Azul',
+            title: 'Croquetas de Queso Azul',
             description: '',
             allergens: [alergenos.gluten.es,alergenos.leche.es,alergenos.huevos.es]
         },
         en: {
-            title: 'BLUE CHEESE',
+            title: 'Croquettes of BLUE CHEESE',
             description: '',
             allergens: [alergenos.gluten.en,alergenos.leche.en,alergenos.huevos.en]
         },
         fr: {
-            title: 'FROMAGE BLEU',
+            title: 'croquettes de FROMAGE BLEU',
             description: "",
             allergens: [alergenos.gluten.fr,alergenos.leche.fr,alergenos.huevos.fr]
         }
     },
     {
-        selectorTitle: '.croquetas .croquetas3 h2',
-        selectorDescription: '.croquetas .croquetas3 .descripcion-plato',
-        selectorAllergens: '.croquetas .croquetas3 .texto-alergenos',
+        selectorTitle: '.raciones .raciones24 h2',
+        selectorDescription: '.raciones .raciones24 .descripcion-plato',
+        selectorAllergens: '.raciones .raciones24 .texto-alergenos',
         es: {
-            title: 'Rabo de Toro',
+            title: 'Croquetas de Rabo de Toro',
             description: '',
             allergens: [alergenos.gluten.es,alergenos.leche.es,alergenos.huevos.es]
         },
         en: {
-            title: 'OX TAIL',
+            title: 'Croquettes of OX TAIL',
             description: '',
             allergens: [alergenos.gluten.en,alergenos.leche.en,alergenos.huevos.en]
         },
         fr: {
-            title: 'QUEUE DE BŒUF',
+            title: 'croquettes de QUEUE DE BŒUF',
             description: "",
             allergens: [alergenos.gluten.fr,alergenos.leche.fr,alergenos.huevos.fr]
         }
     },
     {
-        selectorTitle: '.croquetas .croquetas4 h2',
-        selectorDescription: '.croquetas .croquetas4 .descripcion-plato',
-        selectorAllergens: '.croquetas .croquetas4 .texto-alergenos',
+        selectorTitle: '.raciones .raciones25 h2',
+        selectorDescription: '.raciones .raciones25 .descripcion-plato',
+        selectorAllergens: '.raciones .raciones25 .texto-alergenos',
         es: {
-            title: 'BOLETUS',
+            title: 'Croquetas de BOLETUS',
             description: '',
             allergens: [alergenos.gluten.es,alergenos.leche.es,alergenos.huevos.es]
         },
         en: {
-            title: 'BOLETUS',
+            title: 'Croquettes of BOLETUS',
             description: '',
             allergens: [alergenos.gluten.en,alergenos.leche.en,alergenos.huevos.en]
         },
         fr: {
-            title: 'BOLET',
+            title: 'croquettes de BOLET',
             description: "",
             allergens: [alergenos.gluten.fr,alergenos.leche.fr,alergenos.huevos.fr]
         }
     },
     {
-        selectorTitle: '.croquetas .croquetas5 h2',
-        selectorDescription: '.croquetas .croquetas5 .descripcion-plato',
-        selectorAllergens: '.croquetas .croquetas5 .texto-alergenos',
+        selectorTitle: '.raciones .raciones26 h2',
+        selectorDescription: '.raciones .raciones26 .descripcion-plato',
+        selectorAllergens: '.raciones .raciones26 .texto-alergenos',
         es: {
-            title: 'Jamon (sin gluten)',
+            title: 'Croquetas de Jamon (sin gluten)',
             description: '',
             allergens: [alergenos.leche.es,alergenos.huevos.es]
         },
         en: {
-            title: 'HAM (GLUTEN-FREE)',
+            title: 'Croquettes of HAM (GLUTEN-FREE)',
             description: '',
             allergens: [alergenos.leche.en,alergenos.huevos.en]
         },
         fr: {
-            title: 'JAMBON (SANS GLUTEN)',
+            title: 'croquettes de JAMBON (SANS GLUTEN)',
             description: "",
             allergens: [alergenos.leche.fr,alergenos.huevos.fr]
         }
